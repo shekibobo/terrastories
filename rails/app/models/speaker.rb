ca4270d9c2583fe56a07f43ba07852c24cf94f8d
Sean Marcia <smarcia@gmail.com>
2018-10-02 22:31:44 -0400

    CSV Import

--- rails/app/models/speaker.rb
+++ rails/app/models/speaker.rb
@@ -9,4 +9,16 @@ class Speaker < ApplicationRecord
       ActionController::Base.helpers.image_path('speaker.png', only_path: true)
     end
   end
+
+  def self.import_csv(filename)
+    CSV.parse(filename, headers: true) do |row|
+      speaker = Speaker.where(name: row[0], community: row[2]).first_or_create
+      if row[3] && File.exist?(Rails.root.join('media', row[3]))
+        file = File.open(Rails.root.join('media',row[3]))
+        speaker.media.attach(io: file, filename: row[3])
+        speaker.save
+      end
+    end
+  end
+
 end
--- rails/app/models/story.rb
+++ rails/app/models/story.rb
@@ -5,5 +5,19 @@ class Story < ApplicationRecord
 
   acts_as_taggable
 
+  def self.import_csv(filename)
+    CSV.parse(filename, headers: true) do |row|
+      pointid = Point.where(title: row[3])&.first&.id
+      speakerid = Speaker.where(name: row[2])&.first&.id
+      perm = row[9].blank? ? "anonymous" : "user_only"
+      story = Story.create(title:row[0], point_id: pointid, speaker_id: speakerid, permission_level: perm)
+      if row[8] && File.exist?(Rails.root.join('media', row[8]))
+        file = File.open(Rails.root.join('media',row[8]))
+        story.media.attach(io: file, filename: row[8])
+        story.save
+      end
+    end
+  end
+
   enum permission_level: [:anonymous, :user_only, :editor_only]
 end
